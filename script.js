376233ab67e6834fd30a61486cea2f48f336aea9
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-22 22:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
-let yourArray = ['apple', 42, true, null, 'hello world'];
+let yourArray;
+// assign your array the correct value in the next line
+// your code here!
+yourArray = [5,"three",true,false,null,undefined];
+// code end
 
 module.exports = yourArray
